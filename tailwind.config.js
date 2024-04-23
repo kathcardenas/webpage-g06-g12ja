8802c5a58dacd04945e84153fc621cc916377d48
KathCardenas <kath9704.gmail.com>
2024-04-22 21:18:16 -0600

    style: Configurar Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,15 @@ const {nextui} = require("@nextui-org/react");
 module.exports = {
   content: [
     // ...
+    "./index.html",
+    "./src/**/*.{html,js}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui({})],
 };
