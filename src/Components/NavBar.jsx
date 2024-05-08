c6ae5ba763b25ff0b7f2bb9e627b2dca8fadb9ce
KathCardenas <kath9704.gmail.com>
2024-05-08 13:45:00 -0600

    style: Añadir hover link activo navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,29 +1,49 @@
 import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
+import { useLocation } from "react-router-dom";
 
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Navbar>
-      <NavbarBrand>
-        <Link className="font-bold text-inherit" href="/">
-          INICIO
-        </Link>
+    <Navbar classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-primary",
+        ],
+      }}>
+      <NavbarBrand className="sm:flex gap-4 p-8" justify="center">
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
+      <NavbarContent className="sm:flex gap-4" justify="center">
+      <NavbarItem isActive={location.pathname === "/"}>
+          <Link color="foreground" href="/">
+            INICIO
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={location.pathname === "/sobre-nosotros"}>
           <Link color="foreground" href="/sobre-nosotros">
-            Quienes Somos
+              QUIENES SOMOS
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="/peliculas" aria-current="page">
-            Películas
+        <NavbarItem isActive={location.pathname === "/peliculas"}>
+          <Link color="foreground" href="/peliculas">
+            PELICULAS
           </Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem isActive={location.pathname === "/series"}>
           <Link color="foreground" href="/series">
-            Series
+            SERIES
           </Link>
         </NavbarItem>
       </NavbarContent>
