e662e152130afacde70af072ec2b08c6614ce2eb
KathCardenas <kath9704.gmail.com>
2024-05-11 16:44:15 -0600

    feat: Añadir página series

--- src/Components/Pages/Series.jsx
+++ src/Components/Pages/Series.jsx
@@ -1,8 +1,24 @@
+import CardSeries from "../CarSeries"
+
 export default function App() {
   return (
     <>
     <div className="flex justify-center p-8">
       <h1 className="font-bold text-2xl">SERIES</h1>
+    </div>
+    <div className="px-4 pb-4">
+      <div className="grid grid-cols-4 gap-4">
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+        <div><CardSeries/></div>
+      </div>
     </div>
     </>
  
